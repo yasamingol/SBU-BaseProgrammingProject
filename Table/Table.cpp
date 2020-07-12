7091792462b6f4cca16561cabadaf312dfcd895d
yasamingol <yasamingolzar.ce@gmail.com>
2020-07-12 20:54:00 +0430

    Yac come back to C++

---
+++ Table/Table.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ASUS on 7/12/2020.
+//
+
+#include "Table.h"
---
+++ Table/Table.h
@@ -0,0 +1,14 @@
+//
+// Created by ASUS on 7/12/2020.
+//
+
+#ifndef UNTITLED_TABLE_H
+#define UNTITLED_TABLE_H
+
+
+class Table {
+
+};
+
+
+#endif //UNTITLED_TABLE_H
